8f3329af5e0a35fad9e0ba4c852a4896f973c8bb
n1nni <nin.dautashvili@gmail.com>
2023-09-08 21:19:29 +0400

    add validations to DTO models

--- BookMapperExcepitonMiddleWare/Models/DTO/AuthorDTO.cs
+++ BookMapperExcepitonMiddleWare/Models/DTO/AuthorDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace BookMapperExcepitonMiddleWare.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMapperExcepitonMiddleWare.Models.DTO
 {
     public class AuthorDTO
     {
+        [Required(ErrorMessage ="FirstName is required")]
         public string AuthorFirstName { get; set; }
+        [Required(ErrorMessage ="LastName is required")]
         public string AuthorLastName { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage ="must be greater than 0")]
         public int BookCount { get; set; }
     }
 }
--- BookMapperExcepitonMiddleWare/Models/DTO/BookDTO.cs
+++ BookMapperExcepitonMiddleWare/Models/DTO/BookDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace BookMapperExcepitonMiddleWare.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMapperExcepitonMiddleWare.Models.DTO
 {
     public class BookDTO
     {
+        [Required(ErrorMessage ="Title is required")]
         public string Title { get; set; }
+        [Required(ErrorMessage ="Description is require")]
         public string Description { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage ="Page must be greater than 0")]
         public int TotalPages { get; set; }
     }
 }
